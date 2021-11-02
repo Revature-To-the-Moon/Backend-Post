0e1e973e34870cc5b589a9921626f6b2867e3721
Jean Duong <bduong@mymail.mines.edu>
2021-11-02 13:20:57 -0600

    dependency done

---
+++ BL/PostBL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class PostBl : IBL
+    {
+        private IRepo _repo;
+
+        public PostBl(IRepo irepo)
+        {
+            _repo = irepo;
+        }
+    }
+}
---
+++ DL/DBRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DL
+{
+    public class DBRepo : IRepo
+    {
+        private PostDB _context;
+
+        public DBRepo(PostDB context)
+        {
+            _context = context;
+        }
+    }
+}
